88c17a8a3d458965c18e7b19f41ce8a4d316a659
Colin Williams <colin.williams.seattle@gmailcom>
2019-05-30 17:38:22 -0700

    trying to build a module

---
+++ src/validator.js
@@ -0,0 +1,8 @@
+require("@babel/register");
+
+import { parsePhoneNumberFromString } from 'libphonenumber-js'
+
+export function validation(nationalNumber, iso2) {
+      const phoneNumber = parsePhoneNumberFromString(nationalNumber, iso2);
+      return phoneNumber.isValid()
+    }
---
+++ test/units.js
@@ -0,0 +1,19 @@
+import {validation} from '../src/validator.js'
+
+var expect = require('chai').expect
+
+console.log("Given a valid three letter Country Code and corresponding valid national Phone Number");
+
+var countryCode = "US"
+var phoneNumber = "2063291212"
+
+console.log("When validation(pn,cc) is called")
+
+var validated = validation(phoneNumber,countryCode)
+
+"Then true is returned"
+
+expect(validated).to.be.a('boolean')
+
+
+
